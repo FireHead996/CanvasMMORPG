b9c24f0abd39d6b0cfce387e1fe5f15069d270e9
Mikolaj Panecki <firehead996@gmail.com>
2021-10-01 16:35:27 +0200

    Added map object Added drawMap function Changed FPS counter data to object

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
 
 var ctx = null
 
-var savedSecond = 0, framesLastSecond = 0, framesCounter = 0
+var fpsCounter = {
+    savedSecond: 0,
+    framesLastSecond: 0,
+    framesCounter: 0
+}
+
+var map = {
+    width: 8,
+    height: 8,
+
+    tile: {
+        width: 50,
+        height: 50
+    },
+    
+    matrix: [
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 2, 3, 4, 5, 0, 0, 0,
+        0, 0, 0, 0, 6, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 1, 0,
+        0, 0, 1, 1, 1, 0, 1, 0,
+        0, 0, 0, 0, 1, 0, 1, 0,
+        0, 0, 0, 0, 1, 1, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    ],
+}
+
+function drawMap() {
+    for (let y = 0; y < map.height; y++) {
+        for (let x = 0; x < map.width; x++) {
+            
+            switch(map.matrix[(y * map.width) + x]) {
+                case 1:
+                    ctx.fillStyle = "#10ee05"
+                    break;
+                default: // 0
+                    ctx.fillStyle = "#000000"
+            }
+            
+            ctx.fillRect(x * map.tile.width, y * map.tile.height, map.tile.width, map.tile.height)
+        }
+    }
+}
 
 function drawFrameCounter() {
     let currentSecond = Math.floor(Date.now() / 1000)
 
-    if (currentSecond != savedSecond) {
-        savedSecond = currentSecond
-        framesLastSecond = framesCounter
-        framesCounter = 1;
-    } else { framesCounter++ }
+    if (currentSecond != fpsCounter.savedSecond) {
+        fpsCounter.savedSecond = currentSecond
+        fpsCounter.framesLastSecond = fpsCounter.framesCounter
+        fpsCounter.framesCounter = 1;
+    } else { fpsCounter.framesCounter++ }
 
     ctx.fillStyle = "#ff0000"
-    ctx.fillText("FPS: " + framesLastSecond, 50, 50)
+    ctx.fillText("FPS: " + fpsCounter.framesLastSecond, 50, 50)
 }
 
 function drawGame() {
@@ -22,6 +64,7 @@ function drawGame() {
     ctx.fillStyle = "#ffffff"
     ctx.fillRect(0, 0, 500, 500)
 
+    drawMap()
     drawFrameCounter()
 
     requestAnimationFrame(drawGame)
