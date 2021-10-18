5f34e1541f2cc916e08c8d3df1639e64e2914406
Mikolaj Panecki <firehead996@gmail.com>
2021-10-18 19:49:25 +0200

    Changing global functions to classes with methods

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-
-var ctx = null
-
-var fpsCounter = {
-    savedSecond: 0,
-    framesLastSecond: 0,
-    framesCounter: 0
-}
-
 var map = {
     width: 8,
     height: 8,
@@ -18,8 +9,8 @@ var map = {
     
     matrix: [
         0, 0, 0, 0, 0, 0, 0, 0,
-        0, 2, 3, 4, 5, 0, 0, 0,
-        0, 0, 0, 0, 6, 0, 0, 0,
+        0, 1, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0,
         0, 0, 1, 1, 1, 1, 1, 0,
         0, 0, 1, 1, 1, 0, 1, 0,
         0, 0, 0, 0, 1, 0, 1, 0,
@@ -28,7 +19,7 @@ var map = {
     ],
 }
 
-function drawMap() {
+function drawMap(ctx) {
     for (let y = 0; y < map.height; y++) {
         for (let x = 0; x < map.width; x++) {
             
@@ -37,7 +28,7 @@ function drawMap() {
                     ctx.fillStyle = "#10ee05"
                     break;
                 default: // 0
-                    ctx.fillStyle = "#000000"
+                    ctx.fillStyle = "#000088"
             }
             
             ctx.fillRect(x * map.tile.width, y * map.tile.height, map.tile.width, map.tile.height)
@@ -45,32 +36,66 @@ function drawMap() {
     }
 }
 
-function drawFrameCounter() {
-    let currentSecond = Math.floor(Date.now() / 1000)
+class FrameCounter {
 
-    if (currentSecond != fpsCounter.savedSecond) {
-        fpsCounter.savedSecond = currentSecond
-        fpsCounter.framesLastSecond = fpsCounter.framesCounter
-        fpsCounter.framesCounter = 1;
-    } else { fpsCounter.framesCounter++ }
+    constructor() {
+        this.label = document.querySelector('#fpsCounter')
+        this.savedSecond = 0
+        this.framesLastSecond = 0
+        this.framesCounter = 0
+    }
 
-    ctx.fillStyle = "#ff0000"
-    ctx.fillText("FPS: " + fpsCounter.framesLastSecond, 50, 50)
+    draw = () => {
+        this.label.innerHTML = this.framesLastSecond
+    }
+
+    update = () => {
+        let currentSecond = Math.floor(Date.now() / 1000)
+
+        if (currentSecond != this.savedSecond) {
+            this.savedSecond = currentSecond
+            this.framesLastSecond = this.framesCounter
+            this.framesCounter = 1;
+        } else { this.framesCounter++ }
+    }
 }
 
-function drawGame() {
-    if (ctx == null) { return }
+class Engine {
 
-    ctx.fillStyle = "#ffffff"
-    ctx.fillRect(0, 0, 500, 500)
+    constructor() {
+        this.canvas = document.querySelector('canvas')
+        this.ctx = this.canvas.getContext('2d')
+        this.canvas.width = innerWidth
+        this.canvas.height = innerHeight
 
-    drawMap()
-    drawFrameCounter()
+        this.fpsCounter = new FrameCounter()
+    }
 
-    requestAnimationFrame(drawGame)
+    drawGame = () => {
+        requestAnimationFrame(engine.drawGame)
+
+        // --- Updates and calculations below ---
+
+        this.fpsCounter.update()
+        
+        // --- Drawing below ---
+
+        if (this.ctx == null) { return }
+
+        this.ctx.fillStyle = "rgba(255, 255, 255, 1)"
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+
+        drawMap(this.ctx)
+
+        this.fpsCounter.draw()
+    }
+
 }
+
+// Main Loop
+
+const engine = new Engine()
 
 window.onload = () => {
-    ctx = document.getElementById("game").getContext("2d")
-    requestAnimationFrame(drawGame)
+    requestAnimationFrame(engine.drawGame)
 }
