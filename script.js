ab736a5d7544384ff74acac01c0ff415951ba761
Mikołaj Panecki <firehead996@gmail.com>
2021-10-18 20:54:19 +0200

    Code checkout based on my 2., 3. and 4. YouTube video from MMORPG series (#2)
    * Added map object
    Added drawMap function
    Changed FPS counter data to object
    
    * Changing global functions to classes with methods

--- script.js
+++ script.js
@@ -1,33 +1,101 @@
+var map = {
+    width: 8,
+    height: 8,
 
-var ctx = null
-
-var savedSecond = 0, framesLastSecond = 0, framesCounter = 0
-
-function drawFrameCounter() {
-    let currentSecond = Math.floor(Date.now() / 1000)
-
-    if (currentSecond != savedSecond) {
-        savedSecond = currentSecond
-        framesLastSecond = framesCounter
-        framesCounter = 1;
-    } else { framesCounter++ }
-
-    ctx.fillStyle = "#ff0000"
-    ctx.fillText("FPS: " + framesLastSecond, 50, 50)
+    tile: {
+        width: 50,
+        height: 50
+    },
+    
+    matrix: [
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 1, 0,
+        0, 0, 1, 1, 1, 0, 1, 0,
+        0, 0, 0, 0, 1, 0, 1, 0,
+        0, 0, 0, 0, 1, 1, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    ],
 }
 
-function drawGame() {
-    if (ctx == null) { return }
-
-    ctx.fillStyle = "#ffffff"
-    ctx.fillRect(0, 0, 500, 500)
-
-    drawFrameCounter()
-
-    requestAnimationFrame(drawGame)
+function drawMap(ctx) {
+    for (let y = 0; y < map.height; y++) {
+        for (let x = 0; x < map.width; x++) {
+            
+            switch(map.matrix[(y * map.width) + x]) {
+                case 1:
+                    ctx.fillStyle = "#10ee05"
+                    break;
+                default: // 0
+                    ctx.fillStyle = "#000088"
+            }
+            
+            ctx.fillRect(x * map.tile.width, y * map.tile.height, map.tile.width, map.tile.height)
+        }
+    }
 }
+
+class FrameCounter {
+
+    constructor() {
+        this.label = document.querySelector('#fpsCounter')
+        this.savedSecond = 0
+        this.framesLastSecond = 0
+        this.framesCounter = 0
+    }
+
+    draw = () => {
+        this.label.innerHTML = this.framesLastSecond
+    }
+
+    update = () => {
+        let currentSecond = Math.floor(Date.now() / 1000)
+
+        if (currentSecond != this.savedSecond) {
+            this.savedSecond = currentSecond
+            this.framesLastSecond = this.framesCounter
+            this.framesCounter = 1;
+        } else { this.framesCounter++ }
+    }
+}
+
+class Engine {
+
+    constructor() {
+        this.canvas = document.querySelector('canvas')
+        this.ctx = this.canvas.getContext('2d')
+        this.canvas.width = innerWidth
+        this.canvas.height = innerHeight
+
+        this.fpsCounter = new FrameCounter()
+    }
+
+    drawGame = () => {
+        requestAnimationFrame(engine.drawGame)
+
+        // --- Updates and calculations below ---
+
+        this.fpsCounter.update()
+        
+        // --- Drawing below ---
+
+        if (this.ctx == null) { return }
+
+        this.ctx.fillStyle = "rgba(255, 255, 255, 1)"
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+
+        drawMap(this.ctx)
+
+        this.fpsCounter.draw()
+    }
+
+}
+
+// Main Loop
+
+const engine = new Engine()
 
 window.onload = () => {
-    ctx = document.getElementById("game").getContext("2d")
-    requestAnimationFrame(drawGame)
+    requestAnimationFrame(engine.drawGame)
 }
